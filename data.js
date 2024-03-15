1d0cf38fb538882f6fb5cbb4e519386496b272c6
Abderrahman Beddi <156427004+itsmeabderr@users.noreply.github.com>
2024-03-15 12:11:57 +0000

    update data

--- data.js
+++ data.js
@@ -15,23 +15,6 @@ const envelopes = [
     budget: 2000,
   },
 ];
-const envelopes = [
-  {
-    id: 1,
-    title: "food",
-    budget: 200,
-  },
-  {
-    id: 2,
-    title: "rent",
-    budget: 500,
-  },
-  {
-    id: 3,
-    title: "tech",
-    budget: 2000,
-  },
-];
 
 function createEnvelop(id, title, budget) {
   return {
